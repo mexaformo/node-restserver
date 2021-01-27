a3afd9a6a965a3f4e96d8605b32324869e9127de
Homero <homerooremoh@outlook.com>
2021-01-26 22:55:41 -0400

    Inclsión de variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -12,10 +12,10 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://192.168.8.210:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://sa:KQKxSfw57r9qxAaU@scroposclust.pmu5p.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
-// pass: KQKxSfw57r9qxAaU
+]// pass: KQKxSfw57r9qxAaU
 // mongodb+srv://sa:<password>@scroposclust.pmu5p.mongodb.net/<dbname>?retryWrites=true&w=majority
 
 process.env.URLDB = urlDB;
